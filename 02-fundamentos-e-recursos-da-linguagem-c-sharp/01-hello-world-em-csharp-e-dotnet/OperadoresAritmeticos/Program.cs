1259dc82c902ce2d0b7ef7b67898a7ec0418ecba
karl-no <karl.publicidade@gmail.com>
2024-01-31 12:12:30 -0300

    Operadores aritméticos

---
+++ 02-fundamentos-e-recursos-da-linguagem-c-sharp/01-hello-world-em-csharp-e-dotnet/OperadoresAritmeticos/Program.cs
@@ -0,0 +1,24 @@
+﻿//Adição
+int a = 50, b = 50;
+int result1 = a + b;
+Console.WriteLine(a + " + " + b + " = " + result1);
+
+//Subtração
+int c = 77, d = 21;
+int result2 = c - d;
+Console.WriteLine(c + " - " + d + " = " + result2);
+
+//Multiplicação
+int e = 5, f = 5;
+int result3 = e * f;
+Console.WriteLine(e + " * " + f + " = " + result3);
+
+//divisão
+int g = 90, h = 9;
+int result4 = g / h;
+Console.WriteLine(g + " / " + h + " = " + result4);
+
+//módulo
+int i = 36, j = 7;
+int result5 = i % j;
+Console.WriteLine("O resto da divisão de "+i+" por "+j+" é "+result5);
