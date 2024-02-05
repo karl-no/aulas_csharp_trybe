596ceddec52fea96d252bbd43466b4d6f014e521
karl-no <karl.publicidade@gmail.com>
2024-02-05 18:05:12 -0300

    Exemplo de laço if e conversão entre tipos

---
+++ 02-fundamentos-e-recursos-da-linguagem-c-sharp/02-variaveis-com-tipo-condicionais-e-loops/IfExemplo/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Informe o raio de um círculo (deve ser um número inteiro)");
+        string? response = Console.ReadLine();
+        int radius = 0;
+        var canConvert = Int32.TryParse(response, out radius);
+
+        if(canConvert)
+        {
+            const double pi = 3.14159;
+
+            double circumference = pi * (2 * radius);
+
+            Console.WriteLine("A circunferência de um circulo com raio "+ radius + " é igual a " + circumference);
+        }
+        else
+        {
+            Console.WriteLine("O texto digitado não é um número inteiro.");
+        }
+    }
+}
