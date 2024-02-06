98e8b5e8d665e6df1dbce0be0543ca1c004a8059
karl-no <karl.publicidade@gmail.com>
2024-02-06 16:54:21 -0300

    feat: exemplo de break

---
+++ 02-fundamentos-e-recursos-da-linguagem-c-sharp/02-variaveis-com-tipo-condicionais-e-loops/BreakExemplo/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program {
+  public static void Main() {
+    int numberOfPeople = 0;
+    int numberOfChildren = 0;
+    int age = 99; // Para sempre entrar no looping
+
+    while(age > 0) {
+      Console.WriteLine("Informe a idade da pessoa. Informe 0 para sair.");
+      age = Convert.ToInt32(Console.ReadLine());
+      if (age == 0) break;
+      numberOfPeople++;
+      if (age < 18) numberOfChildren++;
+    }
+
+    double rateOfChildren = 0;
+    if (numberOfChildren > 0) {
+      rateOfChildren = 100 * numberOfChildren / numberOfPeople;
+    }
+
+    Console.WriteLine("Existe " + rateOfChildren + "% de crianças.");
+  }
+}
