2f00d4fde7771c30205f91d3a8cec67dfb6b5a30
karl-no <karl.publicidade@gmail.com>
2024-02-07 14:12:57 -0300

    feat: Exemplo de utilização de arrays

---
+++ 02-fundamentos-e-recursos-da-linguagem-c-sharp/03-arrays-funcoes-e-excecoes/ArraysExemplo/Program.cs
@@ -0,0 +1,30 @@
+﻿﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] expensesCost;
+
+        int numberOfExpenses = getNumberOfExpenses();
+        expensesCost = new int[numberOfExpenses];
+        for (int i = 0; i < numberOfExpenses; i++)
+        {
+            expensesCost[i] = getExpenseCostFromUser();
+        }
+    }
+
+    public static int getNumberOfExpenses()
+    {
+        Console.WriteLine("Entre com o número de despesas: ");
+        string entry = Console.ReadLine();
+        int convertInt = Convert.ToInt32(entry);
+        return convertInt;
+    }
+
+    public static int getExpenseCostFromUser()
+    {
+        Console.WriteLine("Entre com a despesa: ");
+        string entry = Console.ReadLine();
+        int convertInt = Convert.ToInt32(entry);
+        return convertInt;
+    }
+}
