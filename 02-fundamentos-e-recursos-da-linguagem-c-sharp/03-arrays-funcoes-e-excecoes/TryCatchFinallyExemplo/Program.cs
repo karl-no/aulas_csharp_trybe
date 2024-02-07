772785790c4ca162fda328f7cf704557d278d75a
karl-no <karl.publicidade@gmail.com>
2024-02-07 16:45:55 -0300

    feat: try/catch/finally exemplo

---
+++ 02-fundamentos-e-recursos-da-linguagem-c-sharp/03-arrays-funcoes-e-excecoes/TryCatchFinallyExemplo/Program.cs
@@ -0,0 +1,25 @@
+﻿// Dando erro no console, mas o foco aqui é a sintaxe do try/catch/finally
+namespace Program {
+  public class Program {
+    public bool VerifyProductValidity(int productId)
+    {
+      Database db = new Database();
+
+      try {
+        db.Open();
+        var product = db.GetProduct(productId);
+
+        if (product == null){
+          throw new NullReferenceException("Produto não encontrado.");
+        }
+
+        return product.IsValid;
+      } catch (NullReferenceException ex) {
+        Console.WriteLine(ex.Message);
+        throw;
+      } finally {
+        db.Close();
+      }
+    }
+  }
+}
