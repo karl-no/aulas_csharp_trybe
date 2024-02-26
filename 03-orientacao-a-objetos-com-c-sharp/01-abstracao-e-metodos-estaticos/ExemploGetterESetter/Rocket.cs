732944770eb0e0a212aa152e471111e3ad71e0f2
karl-no <karl.publicidade@gmail.com>
2024-02-26 17:11:14 -0300

    feat: exmplos de instância de classe e get e set

---
+++ 03-orientacao-a-objetos-com-c-sharp/01-abstracao-e-metodos-estaticos/ExemploGetterESetter/Rocket.cs
@@ -0,0 +1,18 @@
+﻿namespace ExemploGetterESetter;
+public class Rocket
+{
+  int _fuel;
+  int _maxFuel = 5000;
+
+  public int FuelAsPercentage
+  {
+    get { return _fuel * 100 / _maxFuel; };
+    set {
+      if (value > 100 || value < 0)
+        throw new ArgumentOutOfRangeException();
+      
+      _fuel = value * _maxFuel / 100; 
+      // (return _fuel / _maxFuel) * 100; // Não funciona
+    }
+  }
+}
---
+++ 03-orientacao-a-objetos-com-c-sharp/01-abstracao-e-metodos-estaticos/ExemploInstanciar/Rocket.cs
@@ -0,0 +1,12 @@
+﻿﻿class Rocket
+{
+    string Name { get; set; }
+    int Fuel { get; set; } = 0;
+    decimal Price { get; set; }
+
+    public Rocket(string name, decimal price)
+    {
+        Name = name;
+        Price = price;
+    }
+}
