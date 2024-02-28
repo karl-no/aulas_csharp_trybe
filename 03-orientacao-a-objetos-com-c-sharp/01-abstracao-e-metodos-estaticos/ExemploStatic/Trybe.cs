0f77092ce9f7a78b53a8afaf81171e6b933d20e1
karl-no <karl.publicidade@gmail.com>
2024-02-28 14:09:07 -0300

    feat: exemplos de classe static

---
+++ 03-orientacao-a-objetos-com-c-sharp/01-abstracao-e-metodos-estaticos/ExemploStatic/Trybe.cs
@@ -0,0 +1,25 @@
+﻿public static class Trybe
+{
+    public static string[] students = { "Luiza", "Bruno", "Patrícia", "Tiago" };
+
+    public static string[] Students
+    {
+        get { return students; }
+        set { students = value; }
+    }
+
+    public static int NumberOfStudents
+    {
+        get { return students.Length; }
+    }
+
+    public static bool IsStudent(string name)
+    {
+        return Array.Exists(students, s => s == name);
+    }
+
+    public static void PrintStudents()
+    {
+        Console.WriteLine(string.Join(", ", students));
+    }
+}
